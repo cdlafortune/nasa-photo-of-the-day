c740e91bacd991a118b87a74e478515895a758ec
cdsuddreth <cdsuddreth@gmail.com>
2020-05-01 22:32:27 -0400

    MVP

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React, {useState, useEffect} from "react";
 import "./App.css";
 import axios from "axios";
 import Header from "./Header";
-
+import PhotoCard from "./PhotoCard";
+import styled from "styled-components";
 
 function App() {
+
+  const [myData, setData] = useState([]);
+
 
   useEffect( () => {
     axios.get("https://api.nasa.gov/planetary/apod?api_key=hfO7mr6KV4uEaxxk4yfhx3AzJ2NsJ1wzzNKz38kV")
     .then(response => {
       console.log(response);
+      setData(response.data);
     })
 
     .catch(err => {
@@ -17,10 +22,17 @@ function App() {
     })
   }, []);
 
+
   return (
     <div className="App">
       <Header />
-      {/* <PhotoCard /> */}
+      <PhotoCard 
+          url = {myData.url}
+          date =  {myData.date}
+          title = {myData.title}
+          explanation = {myData.explanation}
+          copyright = {myData.copyright}
+        />
     </div>
   );
 }
--- src/PhotoCard.js
+++ src/PhotoCard.js
@@ -3,5 +3,47 @@ import "./Header.css";
 import styled from "styled-components";
 
 export default function PhotoCard(props){
+    const url = props.url;
+    const date = props.date;
+    const title = props.title;
+    const explanation = props.explanation;
+    const copyright = props.copyright;
 
+
+    const Photo = styled.img`
+        width: 80%;
+        height: auto;
+        margin: 0 auto;
+
+    `;
+
+    const TitleText = styled.h1`
+        font-size: 3rem;
+    `;
+
+    const Card = styled.div`
+        background-color: #1c253c;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-content: center;
+        width: 60%;
+        height: auto;
+        margin: 0 auto;
+        margin-top: 100px;
+        padding: 5%;
+        color: white;
+        font-family: 'Roboto Mono', monospace;
+        font-size: 1rem;
+    `;
+
+    return (
+        <Card>
+            <Photo src= {url} />
+            <TitleText>{title}</TitleText>
+            <p>{date}</p>
+            <p>{copyright}</p>
+            <p>{explanation}</p>
+        </Card>
+    );
 };
